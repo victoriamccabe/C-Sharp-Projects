db3b021f6bf15f3accc1e7104885050954009c5b
victoriamccabe <victoriamccabe96@gmail.com>
2025-09-06 17:09:56 -0600

    Laptop transfer

---
+++ Small Challenges/Guess a Number/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Boolean comparison using a WHILE loop
+        Console.WriteLine("Hello, I am going to count to 20");
+        Thread.Sleep(1000); // Pause for 1000 milliseconds 
+        //Initialize a counter variable
+        int counter = 0;
+
+        // The loop will count until the counter reaches 20
+
+        while (counter <=20)
+        {
+            Console.WriteLine($"Counter: {counter}");
+            counter++; // Increment the counter by 1
+            Thread.Sleep(50); // Pause for 50 milliseconds (0.05 seconds)
+        }
+
+        Console.WriteLine(); // Print an empty line for spacing
+
+        // Boolean comparison using DO WHILE loop
+
+        // Create a new random number generator instance
+        Random random = new Random();
+
+        // Generate a random number between 0 and 20
+        int secretNumber = random.Next(0, 20);
+
+        // Max attempts
+        const int maxAttempts = 3;
+
+        // Boolean to track if the user guessed the number
+        bool guessedCorrectly = false;
+
+
+        Console.WriteLine("My mind got stuck on a particular number. \n Try to guess which number is. You have 3 attemps. \n Ready?\n Guess a number:");
+
+        // Beginning of guessing loop
+        do
+        {
+
+        }
+        
+    }
+}
---
+++ Small Challenges/Learning/Loops/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user to guess a number
+            Console.WriteLine("Guess a number");
+
+            // Read the user's input and convert it to an integer
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Check if the initial guess is correct (12) and set the boolean accordingly
+            bool isGuessed = number == 12;
+
+            // Start a do-while loop that continues until the correct number is guessed
+            do
+            {
+                // Evaluate the user's guess using a switch statement
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again!");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine()); // Ask for a new guess
+                        break;
+
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again!");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine()); // Ask for a new guess
+                        break;
+
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again!");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine()); // Ask for a new guess
+                        break;
+
+                    case 12:
+                        Console.WriteLine("You guessed 12. That is correct!");
+                        isGuessed = true; // Correct guess, exit the loop
+                        break;
+
+                    default:
+                        // Handle all other incorrect guesses
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine()); // Ask for a new guess
+                        break;
+                }
+            }
+
+            // Continue looping while the correct number has not been guessed
+            while (!isGuessed);
+
+            // Wait for user input before closing the console window
+            Console.Read();
+        }
+    }
+}

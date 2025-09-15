942d450148969d2e7e41c75304bcbf5a71ed711e
victoriamccabe <victoriamccabe96@gmail.com>
2025-09-15 13:14:40 -0600

    method

---
+++ Small Challenges/methods.Assigment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace methodAssigment
+{
+    class Program
+    {
+     
+            // Main() is where the program starts running
+            static void Main(string[] args)
+            {
+                // Ask the user to type a number
+                Console.WriteLine("Enter a number:");
+
+                // Convert the input string to an integer
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                // Instance of the Void class
+                Void vm = new Void();
+
+                // Call the DivideByTwo method and pass in the number (This method will print the result to the screen)
+                vm.DivideByTwo(number);
+
+                // Use a method with an output parameter
+                int result;
+                vm.DivideWithOutput(number, out result); // 'out result' will receive the answer
+                Console.WriteLine($"Using output parameter: {number} divided by 2 = {result}");
+
+                // Call overloaded methods (same method name, different parameters)
+                Console.WriteLine($"Overloaded method (int): {vm.OverloadExample(number)}");
+            Console.WriteLine($"Overloaded method (double): {vm.OverloadExample((double)number)}");
+
+            // Use a static class method (no object needed)
+            Console.WriteLine($"Static method result: {Static.MultiplyByTwo(number)}");
+            }
+        }
+    }
+
+       
---
+++ Small Challenges/methods.Assigment/Static.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// A static class cannot be instantiated. All its members must also be static.
+public static class Static
+{
+    // A static method that multiplies a number by 5 and returns it
+    public static int MultiplyByTwo(int number)
+    {
+        return number * 5;
+    }
+}
